c950998f57f82ab18e78bc5bce5311d2aa723875
ndtien142 <ndtien142@users.noreply.github.com>
2024-11-24 19:03:08 +0000

    deploy: 426bcc862e1ff186b2b42f354b5947e8e3180fe0

---
+++ assets/LayoutAuth-CQwrb9cZ.js
@@ -0,0 +1,1 @@
+import{j as s,O as o}from"./index-CwXXaYY6.js";const t=()=>s.jsx("div",{className:"no-scrollbar overflow-scroll",children:s.jsx(o,{})});export{t as default};
--- assets/LayoutAuth-TM350Ywn.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as o}from"./index-ByETNTAB.js";const t=()=>s.jsx("div",{className:"no-scrollbar overflow-scroll",children:s.jsx(o,{})});export{t as default};
